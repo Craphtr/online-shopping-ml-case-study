865b1a64d85efdbe15fb1174a02c5908292c2370
Jubril Davies <jyde@protonmail.com>
2024-12-13 13:27:02 +0100

    committed 2 behavioural indicators

--- src/functions/feature_engineering.py
+++ src/functions/feature_engineering.py
@@ -22,22 +22,30 @@ def feature_engineering(cleaned_prepd_shopping,
 
     # Add New Engineered Features   
 
-    #1. User Engagement Features
-    #Session_duration captures the total time spent during a session
+#1. User Engagement Features
+    #1.1. Session_duration captures the total time spent during a session
     prepd_and_engineered_shopping['session_duration'] = prepd_and_engineered_shopping[['administrative_duration','informational_duration','product_related_duration']].sum(axis=1)
 
-    #Engagement_Intensity computes the ratio of page_values/session_duration to reflect how valuable the session was relative to the time spent
+    #1.2. Engagement_Intensity computes the ratio of page_values/session_duration to reflect how valuable the session was relative to the time spent
     if apply_compute_engagement:
         prepd_and_engineered_shopping['engagement_intentsity'] = prepd_and_engineered_shopping.apply(compute_engagement,axis=1)
+        print("Number of Null values in Engagement Intensity", prepd_and_engineered_shopping['engagement_intensity'].isnull().sum())
+
+#2. Behavioural Indicators
+    #2.1. Info_Explorer captures time spent exploring info related pages during the session duration
+    prepd_and_engineered_shopping['info_explorer'] = prepd_and_engineered_shopping['informational_duration']/prepd_and_engineered_shopping['session_duration'].replace([np.inf,-np.inf],1e4).fillna(0)
+
+    #2.2. Product Explorer captures value to indicate purchase intent
+    prepd_and_engineered_shopping['product_explorer'] = prepd_and_engineered_shopping['product_related_duration']/prepd_and_engineered_shopping['session_duration'].replace([np.inf,-np.inf], 1e6).fillna(0)
+
+    #2.3. Interaction Strength captures fraction of time spent between exporing and product interest
+    
 
 
 
 
 
-
-
-
-
+#Compute Engagement
 def compute_engagement(row):
     if row['session_duration'] == 0:
         if row['purchase'] == 1:
@@ -45,3 +53,7 @@ def compute_engagement(row):
         else:
             return 0 #Assign 0 for a likely bounce
     else: return row['page_values']/row['session_duration']
+
+#Compute Info Explorer
+def compute_info_explorer(row):
+    if row['informational_duration']/row['session_duration'] == np.inf()
