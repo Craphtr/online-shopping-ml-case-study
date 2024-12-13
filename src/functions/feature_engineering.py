5c215dacfe2896aff5ab08f787ccae529bd5b25f
Jubril Davies <jyde@protonmail.com>
2024-12-13 12:00:53 +0100

    added feature engineering to the pipeline

---
+++ src/functions/feature_engineering.py
@@ -0,0 +1,23 @@
+# feature_engineering.py
+import numpy as np
+import pandas as pd
+
+def feature_engineering(cleaned_prepd_shopping):
+    """
+    This function performs feature engineering on the cleaned and preprocessed shopping data
+    
+    Parameters:
+    cleaned_prepd_shopping (DataFrame): cleaned and preprocessed shopping data
+    
+    Returns: 
+    prepd_and_engineered_shopping (DataFrame): preprocessed and engineered data
+     
+    Usage:
+    prepd_and_engineered_shopping = feature_engineering(cleaned_prepd_shopping):
+    """
+    prepd_and_engineered_shopping = cleaned_prepd_shopping.copy()
+
+    
+    #1. Add New Enginered Features   
+    prepd_and_engineered_shopping['session_duration'] = prepd_and_engineered_shopping[['administrative_duration','informational_duration','product_related_duration']].sum(axis=1)
+    prepd_and_engineered_shopping['engagement_intentsity'] = (prepd_and_engineered_shopping['page_values']/prepd_and_engineered_shopping['session_duration']).fillna(0)
