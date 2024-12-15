66534884d4662a2d216545cec8d27eb4500f1430
Jubril Davies <jyde@protonmail.com>
2024-12-15 11:13:46 +0100

    removed engagement intensity

--- src/functions/feature_engineering.py
+++ src/functions/feature_engineering.py
@@ -26,21 +26,24 @@ def feature_engineering(cleaned_prepd_shopping):
     prepd_and_engineered_shopping['session_duration'] = prepd_and_engineered_shopping[['administrative_duration','informational_duration','product_related_duration']].sum(axis=1)
 
     #1.2. Engagement_Intensity computed tp predict user's intention as interaction between page_values, product_related_duration and informational_duration
-    prepd_and_engineered_shopping['engagement_intentsity'] = prepd_and_engineered_shopping['product_related_duration']*prepd_and_engineered_shopping['page_values']*prepd_and_engineered_shopping['informational_duration']
-    print("Number of Null values in Engagement Intensity", prepd_and_engineered_shopping['engagement_intensity'].isnull().sum())
+    #prepd_and_engineered_shopping['engagement_intensity'] = prepd_and_engineered_shopping['product_related_duration']*prepd_and_engineered_shopping['page_values']*prepd_and_engineered_shopping['informational_duration']
+    #print("Number of Null values in Engagement Intensity", prepd_and_engineered_shopping['engagement_intensity'].isnull().sum())
 
 #2. Behavioural Indicators
     #2.1. Info_Explorer captures time spent exploring info related pages during the session duration
-    prepd_and_engineered_shopping['info_explorer'] = prepd_and_engineered_shopping['informational_duration']/prepd_and_engineered_shopping['session_duration'].replace([np.inf,-np.inf],1e6).fillna(0)
+    prepd_and_engineered_shopping['info_explorer'] = prepd_and_engineered_shopping['informational_duration'] / prepd_and_engineered_shopping['session_duration'].replace([np.inf,-np.inf],prepd_and_engineered_shopping['informational_duration']).fillna(0)
+    print("Number of Null values in info_explorer", prepd_and_engineered_shopping['info_explorer'].isnull().sum())
 
     #2.2. Product Explorer captures value to indicate purchase intent
-    prepd_and_engineered_shopping['product_explorer'] = prepd_and_engineered_shopping['product_related_duration']/prepd_and_engineered_shopping['session_duration'].replace([np.inf,-np.inf], 1e6).fillna(0)
+    prepd_and_engineered_shopping['product_explorer'] = prepd_and_engineered_shopping['product_related_duration'] / prepd_and_engineered_shopping['session_duration'].replace([np.inf,-np.inf], prepd_and_engineered_shopping['product_related_duration']).fillna(0)
+    print("Number of Null values in product_explorer", prepd_and_engineered_shopping['product_explorer'].isnull().sum())
 
     #2.3. Interaction Strength captures fraction of time spent between exploring and product interest
-    prepd_and_engineered_shopping['interaction_strength'] = prepd_and_engineered_shopping['informational_duration'] * prepd_and_engineered_shopping['product_related_duration']
+    prepd_and_engineered_shopping['interaction_strength'] = prepd_and_engineered_shopping['informational_duration'] + prepd_and_engineered_shopping['product_related_duration']
+    print("Number of Null values in interaction_strength", prepd_and_engineered_shopping['interaction_strength'].isnull().sum())
 
     #2.4. Interaction Depth Are users going to more of product pages or spending more time learning 
-    prepd_and_engineered_shopping['interaction_depth'] = prepd_and_engineered_shopping['product_related'] / prepd_and_engineered_shopping['informational']
+    prepd_and_engineered_shopping['interaction_depth'] = prepd_and_engineered_shopping['product_related'] / prepd_and_engineered_shopping['informational'].replace([np.inf,-np.inf], prepd_and_engineered_shopping['product_related'])
 
     #2.5. Adjusted Bounce Rate weights bounce rate with session duration for a more realistic interpretation
     prepd_and_engineered_shopping['adjusted_bounce_rate'] = prepd_and_engineered_shopping['bounce_rates'] * prepd_and_engineered_shopping['session_duration']
